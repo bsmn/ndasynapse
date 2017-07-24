e9f96a491ba86df07290fbcbd37e1362d46453d3
kdaily <kenneth.daily@sagebase.org>
2017-07-24 11:53:55 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ndasynapse',
+      version='0.1',
+      description='NDA to Synapse sync',
+      url='http://github.com/bsmn/ndasynapse',
+      author='Kenneth Daily',
+      author_email='kenneth.daily@sagebase.org',
+      license='MIT',
+      packages=['ndasynapse'],
+      zip_safe=False)
