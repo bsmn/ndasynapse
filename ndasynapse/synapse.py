44e8c62c7f63cdb328d463400410b9d3bc00a7f6
kdaily <kenneth.daily@sagebase.org>
2017-08-10 13:25:36 -0700

    create file handles

--- ndasynapse/synapse.py
+++ ndasynapse/synapse.py
@@ -11,23 +11,20 @@ pandas.options.display.max_rows = None
 pandas.options.display.max_columns = None
 pandas.options.display.max_colwidth = 1000
 
-logger = logging.getLogger("synapse").setLevel(logging.DEBUG)
-
-# NDA Configuration
-REFERENCE_GUID = 'NDAR_INVRT663MBL'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
 
 # Synapse configuration
 import synapseclient
 synapse_data_folder = 'syn7872188'
 synapse_data_folder_id = int(synapse_data_folder.replace('syn', ''))
-storage_location_id = '9209'
-
-content_type_dict = {'.gz': 'application/x-gzip', '.bam': 'application/octet-stream',
-                     '.zip': 'application/zip'}
 
 dry_run = False
 
-def create_synapse_filehandles(metadata_manifest, bucket_name):
+def create_synapse_filehandles(syn, metadata_manifest, bucket_name, storage_location_id):
     """Create a list of Synapse file handles (S3FileHandles) to link to."""
 
     fh_list = []
@@ -38,8 +35,6 @@ def create_synapse_filehandles(metadata_manifest, bucket_name):
         contentSize = x['size']
         contentMd5 = x['md5']
 
-        logger.debug("%s - %s" % (s3Key, s3FilePath))
-
         # Check if it exists in Synapse
         res = syn.restGET("/entity/md5/%s" % (contentMd5, ))['results']
 
@@ -47,25 +42,28 @@ def create_synapse_filehandles(metadata_manifest, bucket_name):
 
         if len(res) > 0:
             fhs = [syn.restGET("/entity/%(id)s/version/%(versionNumber)s/filehandles" % er) for er in res]
-            fileHandleObj = syn._getFileHandle(fhs[0]['list'][0]['id'])
+            fileHandle = syn._getFileHandle(fhs[0]['list'][0]['id'])
         else:
             contentType = content_type_dict.get(os.path.splitext(x['filename'])[-1],
                                                 'application/octet-stream')
 
             fileHandle = {'concreteType': 'org.sagebionetworks.repo.model.file.S3FileHandle',
-                          'fileName'    : s3FilePath,
-                          'contentSize' : contentSize,
-                          'contentType' : contentType,
-                          'contentMd5' :  contentMd5,
-                          'bucketName' : NDA_BUCKET_NAME,
-                          'key'        : s3Key,
-                          'storageLocationId' : storage_location_id}
+            'fileName'    : s3FilePath,
+            'contentSize' : contentSize,
+            'contentType' : contentType,
+            'contentMd5' :  contentMd5,
+            'bucketName' : NDA_BUCKET_NAME,
+            'key'        : s3Key,
+            'storageLocationId' : storage_location_id}
 
-            fileHandleObj = syn.restPOST('/externalFileHandle/s3',
-                                         json.dumps(fileHandle),
-                                         endpoint=syn.fileHandleEndpoint)
+            logger.debug("Doesn't exist: %s - %s" % (s3Key, s3FilePath))
 
-        fh_list.append(fileHandleObj)
+
+            # fileHandle = syn.restPOST('/externalFileHandle/s3',
+            #                           json.dumps(fileHandle),
+            #                           endpoint=syn.fileHandleEndpoint)
+
+        fh_list.append(fileHandle)
 
     return fh_list
 
