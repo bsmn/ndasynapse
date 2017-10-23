35be46487823ab4a3507eb9ffbff0e750f571645
kdaily <kdaily@users.noreply.github.com>
2017-10-23 14:48:14 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ndasynapse',
-      version='0.2.0',
+      version='0.3.0',
       description='NDA to Synapse sync',
       url='http://github.com/bsmn/ndasynapse',
       author='Kenneth Daily',
